c43d85dacc821739b72d98e1e15a9d57cfcf83a9
Alex <arulko@yandex.ru>
2010-05-08 13:50:32 +0400

    Added parsing command line args

--- exps.rb
+++ exps.rb
@@ -3,17 +3,17 @@
 exp_num = 0
 #iterate over 5 diff. params to make runs
 #default params
-#NP = 8191			#Particle number
-NP=131071
+NP = 16635			#Particle number
+#NP=131071
 M = 1.0			#Solar mass
-m = M*10**(-1)		#Earth mass
-R = 1.0			#AU
+m = M*0.1		#Earth mass
+R = 1.0		#AU
 disp = 0.0		#Vel dispersion
-r_a = 0.05		#Earth radius(width, along R)
-r_b = 0.01		#Earth radius(height(
+r_a = 0.05*R		#Earth radius(width, along R)
+r_b = 0.01*R		#Earth radius(height(
 exp_dir = Dir.pwd.to_s
 # m
-5.times{|i|
+4.times{|i|
 	Dir.chdir("#{exp_dir}")
 	Dir.mkdir("#{exp_num}")
 	`cp generate_init.rb #{exp_num}`
@@ -25,9 +25,9 @@ exp_dir = Dir.pwd.to_s
 	puts "#{exp_dir} #{curr_dir}"
 	Dir.chdir("#{curr_dir}")
 	Dir.mkdir("dump")
-	`./generate_init.rb #{NP} #{m*(2**i)} #{disp} #{r_a} #{r_b}`
+	`./generate_init.rb NP=#{NP} M=#{M} R=#{R} m=#{m*(2**i)} disp=#{disp} r_a=#{r_a} r_b=#{r_b}`
 	`./main input.data 0.0`
-	`asplash dump/*.dump -x 2 -y 3 -dev /png`
+	`asplash dump/*.dump -x 2 -y 3 -r 7 -dev /png`
 	`./fixpgplotnames.bash`
 #	`rm *.dump`
 	exp_num += 1
